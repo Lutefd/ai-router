7da2f36ce6ff05c9bf4d19643a448744a15f6606
Luis Dourado <lfaddourado@gmail.com>
2024-08-23 12:56:21 -0300

    feat(openapi): add scalar to display openapi spec and configure fastapi to serve it

--- app/http_gateway.py
+++ app/http_gateway.py
@@ -5,10 +5,15 @@ from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 from typing import Dict, Optional
 from app import ai_router_pb2, ai_router_pb2_grpc
+from scalar_fastapi import get_scalar_api_reference
+from fastapi.responses import FileResponse, JSONResponse
+import yaml
+from pathlib import Path
+import os
 
 logger = logging.getLogger(__name__)
 
-app = FastAPI()
+app = FastAPI(openapi_url=None)
 
 class AIRequest(BaseModel):
     provider: str
@@ -63,6 +68,28 @@ async def stream(request: AIRequest):
 
     return StreamingResponse(response_stream(), media_type="text/event-stream")
 
+@app.get("/openapi.yaml", include_in_schema=False)
+async def serve_openapi_yaml():
+    file_path = "/app/docs/swagger/swagger.yaml"
+    logger.debug(f"Attempting to serve file from: {file_path}")
+
+    if os.path.exists(file_path):
+        logger.debug(f"File found at {file_path}")
+        try:
+            return FileResponse(file_path, media_type="application/x-yaml")
+        except Exception as e:
+            logger.error(f"Error serving file: {str(e)}")
+            return JSONResponse(status_code=500, content={"error": f"Error serving file: {str(e)}"})
+    else:
+        logger.error(f"File not found at {file_path}")
+        return JSONResponse(status_code=404, content={"error": f"File not found at {file_path}"})
+
+@app.get("/reference", include_in_schema=False)
+async def scalar_html():
+    return get_scalar_api_reference(
+        openapi_url= "/openapi.yaml",
+        title=app.title,
+    )
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
